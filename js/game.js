796126113c90b4124afadd17b81e85bbe548346d
Ole-web <porath.ole@gmail.com>
2024-06-28 12:48:22 +0200

    Solved lab

--- js/game.js
+++ js/game.js
@@ -1,3 +1,78 @@
 class Game {
-    // code to be added
+  constructor() {
+    this.startScreen = document.getElementById("game-intro");
+    this.gameScreen = document.getElementById("game-screen");
+    this.gameEndScreen = document.getElementById("game-end");
+
+    this.player = null;
+    this.height = 600;
+    this.width = 500;
+    this.obstacles = [];
+    this.score = 0;
+    this.lives = 3;
+    this.gameIsOver = false;
+    this.gmaeIntervalId;
+    this.gmaeLoopFrequency = Math.round(1000 / 60);
+
+    this.player = new Player(
+      this.gameScreen,
+      200,
+      500,
+      100,
+      150,
+      "images/car.png"
+    );
+  }
+
+  start() {
+    this.startScreen.style.display = "none";
+    this.gameScreen.style.width = this.width + "px";
+    this.gameScreen.style.height = this.height + "px";
+    this.gameScreen.style.display = "block";
+
+    this.gmaeIntervalId = setInterval(() => {
+      this.gameLoop();
+    }, this.gmaeLoopFrequency);
+  }
+  gameLoop() {
+    if (this.gameIsOver) {
+      clearInterval(this.gmaeIntervalId);
+      this.gameEnd();
+    } else {
+      this.update();
+    }
+  }
+  update() {
+    this.player.move();
+
+    for (let i = 0; i < this.obstacles.length; i++) {
+      const obstacle = this.obstacles[i];
+      obstacle.move();
+
+      if (this.player.didCollide(obstacle)) {
+        obstacle.element.remove();
+        this.obstacles.splice(i, 1);
+        this.lives--;
+        i--;
+      } else if (obstacle.top > this.height) {
+        this.score++;
+        obstacle.element.remove();
+        this.obstacles.splice(i, 1);
+        i--;
+      }
+    }
+    if (this.lives === 0) {
+      this.endGame();
+    }
+    if (Math.random() > 0.98 && this.obstacles.length < 1) {
+      this.obstacles.push(new Obstacle(this.gameScreen));
+    }
+  }
+  endGame() {
+    this.player.element.remove();
+    this.obstacles.forEach((obstacle) => obstacle.element.remove());
+    this.gameIsOver = true;
+    this.gameScreen.style.display = "none";
+    this.gameEndScreen.style.display = "block";
+  }
 }
--- js/script.js
+++ js/script.js
@@ -1,12 +1,51 @@
 window.onload = function () {
   const startButton = document.getElementById("start-button");
   const restartButton = document.getElementById("restart-button");
+  let game;
+
+  function handleKeydown() {
+    const key = Event.key();
+    const possibleKeystrokes = [
+      "ArrowLeft",
+      "ArrowRight",
+      "ArrowUp",
+      "ArrowDown",
+    ];
+    if (possibleKeystrokes.includes(key)) {
+      Event.preventDefault();
+      switch (key) {
+        case "ArrowLeft":
+          game.player.directionX = -1;
+          break;
+        case "ArrowRight":
+          game.player.directionX = 1;
+          break;
+        case "ArrowUp":
+          game.player.directionY = -1;
+          break;
+        case "ArrowDown":
+          game.player.directionY = 1;
+      }
+    }
+  }
+
+  window.addEventListener("keydown", handleKeydown);
 
   startButton.addEventListener("click", function () {
     startGame();
   });
+  restartButton.addEventListener("click", function () {
+    restartGame();
+  });
 
   function startGame() {
     console.log("start game");
+    game = new Game();
+
+    game.start();
+  }
+
+  function restartGame() {
+    location.reload();
   }
 };
